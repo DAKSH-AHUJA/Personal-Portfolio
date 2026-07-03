eb13c49bedb098a6d8717f9f4c76f9c378c9c10a
Daksh <dakshahuja31106@gmail.com>
2026-07-03 14:33:45 +0530

    changed the name style

--- app/components/hero/index.tsx
+++ app/components/hero/index.tsx
@@ -35,7 +35,7 @@ const Hero = () => {
 
   return (
     <>
-      <Text position={[0, 2, -10]} {...fontProps} ref={titleRef}>Hi, I am DAKSH AHUJA.</Text>
+      <Text position={[0, 2, -10]} {...fontProps} ref={titleRef}>Hi, This is Daksh Ahuja.</Text>
       <StarsContainer />
       <CloudContainer/>
       <group position={[0, -25, 5.69]}>
